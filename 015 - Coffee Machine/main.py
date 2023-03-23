1ab6cfb160518b4c6c68070518c34e7c8d33f26c
Michelle Tan <michelletanjm@gmail.com>
2023-03-23 17:37:13 +0800

    Day 15 - Complete Coffee Machine

--- 015 - Coffee Machine/main.py
+++ 015 - Coffee Machine/main.py
@@ -1,7 +1,15 @@
-import os
-from till import Money, Till
+from enum import Enum
 
 # Constants
+
+
+class Money(Enum):
+    QUARTER = 0
+    DIME = 1
+    NICKEL = 2
+    PENNY = 3
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -28,6 +36,7 @@ MENU = {
     }
 }
 
+
 # State Variables
 
 
@@ -37,13 +46,15 @@ resources = {
     "coffee": 100,
 }
 
-current_till = Till()
+current_money = 0
 
 # Prompt user for their order (3 types of coffee)
 
 
 def process_user_input():
-    action = input("What would you like? (espresso/latte/cappucino): ").lower()
+    global current_money
+    action = input(
+        "What would you like? (espresso/latte/cappuccino): ").lower()
 
     if action == "off":
         print("Switching off...")
@@ -52,25 +63,50 @@ def process_user_input():
         print_report()
         return
     elif action in MENU.keys():
+        drink_name = action
         drink = MENU[action]
 
-        # Check if resources are sufficient
+        if not is_resources_enough(drink):
+            # If there are not enough resources, prompt user again
+            return
 
         # Ask for coins
-        coins = receive_coins()
-        if coins.get_total_value() < drink["cost"]:
-            print("Not enough money.")
-        return
+        print(f"Please insert {format_money_string(drink['cost'])} in coins.")
+        received_money = receive_coins()
+        if received_money < drink["cost"]:
+            print(
+                f"Sorry, that's not enough. Refunding {format_money_string(received_money)}...")
+            return
+        else:
+            if received_money > drink["cost"]:
+                refund = round(received_money - drink['cost'], 2)
+                print(
+                    f"Returning {format_money_string(refund)} in change...")
+            current_money += drink["cost"]
+
+        # Make the drink
+        make_drink(drink)
+        print(f"Here's your {drink_name}, enjoy!")
     else:
         print(f"Sorry, we don't serve {action}.")
 
 
 def print_report():
-    global resources, current_till
+    global resources, current_money
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
-    print(f"Money: ${(current_till.get_total_value())}")
+    print(f"Money: {format_money_string(current_money)}")
+
+
+def is_resources_enough(drink):
+    global resources
+    recipe = drink["ingredients"]
+    for k in recipe.keys():
+        if resources[k] < recipe[k]:
+            print(f"Sorry, there is not enough {k}.")
+            return False
+    return True
 
 
 def receive_coins():
@@ -79,7 +115,12 @@ def receive_coins():
     n = sanitise_coin_input(input("How many nickels?: "))
     p = sanitise_coin_input(input("How many pennies?: "))
 
-    return Till([q, d, n, p])
+    return get_total_value({
+        Money.QUARTER: q,
+        Money.DIME: d,
+        Money.NICKEL: n,
+        Money.PENNY: p
+    })
 
 
 def sanitise_coin_input(x):
@@ -89,10 +130,25 @@ def sanitise_coin_input(x):
         return 0
 
 
+def format_money_string(x):
+    """Converts number to string with 2 decimal places and adds $"""
+    return "${:.2f}".format(x)
+
+
+def get_total_value(coins):
+    q = coins[Money.QUARTER] * 0.25
+    d = coins[Money.DIME] * 0.1
+    n = coins[Money.NICKEL] * 0.05
+    p = coins[Money.PENNY] * 0.01
+    return q + d + n + p
+
+
+def make_drink(drink):
+    global resources
+    recipe = drink["ingredients"]
+    for k in recipe.keys():
+        resources[k] -= recipe[k]
+
+
 while True:
     process_user_input()
-
-# 4. Check if machine has enough resources before producing coffee
-# 5. Process coins given by user
-# 6. Check if transaction is successful ie. user has given enough money
-# 7. Make coffee - use up resources and output the chosen coffee
