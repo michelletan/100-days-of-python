ff2e3915b578bf34d42bb29813633fb576393155
Michelle Tan <michelletanjm@gmail.com>
2023-04-04 21:09:45 +0800

    Day 29 - Complete assignment

--- 029 - Password Manager/README.md
+++ 029 - Password Manager/README.md
@@ -2,5 +2,8 @@
 
 Task: Create a password manager that can save passwords locally and generate passwords.
 
+## Screenshot
+![Screenshot of a password manager](example.png)
+
 Learning points:
 - GUI with Tkinter
--- 029 - Password Manager/main.py
+++ 029 - Password Manager/main.py
@@ -1,19 +1,49 @@
-from tkinter import Tk, Canvas, PhotoImage, Label, Entry
+import pyperclip
+from tkinter import Tk, Canvas, PhotoImage, Label, Entry, messagebox, END
 from tkmacosx import Button
+from password_generator import generate_password
 
 FONT = ("Arial", 20)
 GRID_PADDING_Y = (0, 10)
+DEFAULT_EMAIL = "michelle@github.com"
+OUTPUT_FILEPATH = "passwords.csv"
+
+# ---------------------------- HELPER METHODS ------------------------------- #
+
+
+def clear_inputs(*inputs):
+    for i in inputs:
+        i.delete(0, END)
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
-def generate_password():
-    pass
+def get_password():
+    global password_entry
+    pw = generate_password()
+    password_entry.insert(0, pw)
+    pyperclip.copy(pw)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_password():
-    pass
+    global website_entry, email_entry, password_entry
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if not website or not email or not password:
+        messagebox.showwarning(
+            title="Oops", message="Don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"These are the details entered:\n Email:{email}\n Password: {password}\n Is it ok to save?")
+
+        if is_ok:
+            with open(OUTPUT_FILEPATH, mode="a") as save_file:
+                save_file.write(f"{website}, {email}, {password}\n")
+
+            clear_inputs(website_entry, password_entry)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -33,12 +63,14 @@ website_label.grid(row=1, column=0, pady=GRID_PADDING_Y)
 
 website_entry = Entry()
 website_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
+website_entry.focus()
 
 email_label = Label(text="Email:", anchor="e", font=FONT)
 email_label.grid(row=2, column=0, pady=GRID_PADDING_Y)
 
 email_entry = Entry()
 email_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
+email_entry.insert(0, DEFAULT_EMAIL)
 
 password_label = Label(text="Password:", anchor="e", font=FONT)
 password_label.grid(row=3, column=0, pady=GRID_PADDING_Y)
@@ -46,7 +78,7 @@ password_label.grid(row=3, column=0, pady=GRID_PADDING_Y)
 password_entry = Entry()
 password_entry.grid(row=3, column=1, sticky="ew")
 
-password_btn = Button(text="Generate Password", command=generate_password)
+password_btn = Button(text="Generate Password", command=get_password)
 password_btn.grid(row=3, column=2, sticky="ew")
 
 add_btn = Button(text="Add", command=save_password)
